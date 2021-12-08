87b47ea0d17faa64e0c20b75c8372858ede5b82f
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-09 00:02:43 +0900

    説明用モーダル追加

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,12 @@ $(function () {
     $('#upload').change(function () {
         $("#btn_submit").click();
     });
+
+    $('#btn_explanation').click(function () {
+        $('#signup_modal').fadeIn();
+    });
+
+    $('.close_icon').click(function () {
+        $('#signup_modal').fadeOut();
+    });
 });
