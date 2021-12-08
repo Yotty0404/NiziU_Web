68e84f4e58dc8db0057565a43a2b936db08c4444
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-09 07:16:16 +0900

    画像を上げると、動的に値を埋め込む処理追加

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def allwed_file(filename):
 
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
+    data = [[],[],[]]
 # リクエストがポストかどうかの判別
     if request.method == 'POST':
         # ファイルがなかった場合の処理
@@ -30,9 +31,10 @@ def upload_file():
             # 危険な文字を削除（サニタイズ処理）
             filename = secure_filename(file.filename)
             print('OK')
-        return render_template('index.html')
+            data = [['tesuto','72.3%'],['test2','52.1%'],['test3','35.5%']]
+        return render_template('index.html', data=data)
     else:
-    	return render_template('index.html')
+        return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
