333970a6b95563c3700f57e098025a849ecae3f8
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-09 08:07:43 +0900

    loading画面追加

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,25 @@
+function sleep(waitSec, callbackFunc) {
+    // 経過時間（秒）
+    var spanedSec = 0;
+
+    // 1秒間隔で無名関数を実行
+    var id = setInterval(function () {
+
+        spanedSec++;
+
+        // 経過時間 >= 待機時間の場合、待機終了。
+        if (spanedSec >= waitSec) {
+
+            // タイマー停止
+            clearInterval(id);
+
+            // 完了時、コールバック関数を実行
+            if (callbackFunc) callbackFunc();
+        }
+    }, 1000);
+
+}
+
 $(function () {
     $("#btn_upload").on("click", function (event) {
         $("#upload").click();
@@ -5,8 +27,13 @@ $(function () {
     });
 
     $('#upload').change(function () {
-        $('#signup_modal').fadeIn(500);
-        $("#btn_submit").click();
+        $('#mdl_loading').fadeIn();
+
+
+        sleep(0.5, function () {
+            $("#btn_submit").click();
+        });
+
     });
 
     $('#btn_explanation').click(function () {
