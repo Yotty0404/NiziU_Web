e5dfd19a42ad0594ba0e4ed57616c80acf5260fc
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-09 17:31:27 +0900

    iphoneのスタイルが崩れる問題対応

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
     data = [[],[],[]]
+    lbl = ''
 # リクエストがポストかどうかの判別
     if request.method == 'POST':
         # ファイルがなかった場合の処理
@@ -24,10 +25,11 @@ def upload_file():
             # 危険な文字を削除（サニタイズ処理）
             filename = secure_filename(file.filename)
             print('OK')
+            lbl = 'そっくり度...'
             data = [['tesuto','72.3%'],['test2','52.1%'],['test3','35.5%']]
-        return render_template('index.html', data=data)
+        return render_template('index.html', lbl=lbl, data=data)
     else:
-        return render_template('index.html', data=data)
+        return render_template('index.html', lbl=lbl, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
